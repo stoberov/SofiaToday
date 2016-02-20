54c8d8e57adba9f0686a870e1d90de53692d8daa
Stoyan <stoyan.berov@gmail.com>
2016-02-20 16:43:03 +0200

    Added Kendo + DateTimePickers

--- Source/Web/SofiaToday.Web/App_Start/BundleConfig.cs
+++ Source/Web/SofiaToday.Web/App_Start/BundleConfig.cs
@@ -4,7 +4,6 @@
 
     public static class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             RegisterScripts(bundles);
@@ -16,11 +15,13 @@
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include("~/Scripts/KendoUI/kendo.all.min.js", "~/Scripts/KendoUI/kendo.aspnetmvc.min.js"));
         }
 
         private static void RegisterStyles(BundleCollection bundles)
         {
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/kendo").Include("~/Content/KendoUI/kendo.common-bootstrap.min.css", "~/Content/KendoUI/kendo.bootstrap.min.css"));
         }
     }
 }
