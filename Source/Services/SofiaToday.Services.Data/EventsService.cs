4af5720df103b94899dae3aaf1393c877659ff8a
Stoyan <stoyan.berov@gmail.com>
2016-02-20 16:11:55 +0200

    Event Service

---
+++ Source/Services/SofiaToday.Services.Data/EventsService.cs
@@ -0,0 +1,49 @@
+﻿namespace SofiaToday.Services.Data
+{
+    using System;
+    using System.Linq;
+
+    using SofiaToday.Data.Common;
+    using SofiaToday.Data.Models;
+
+    public class EventsService : IEventsService
+    {
+        private readonly IDbRepository<Event> events;
+
+        public EventsService(IDbRepository<Event> events)
+        {
+            this.events = events;
+        }
+
+        public IQueryable<Event> GetAll()
+        {
+            return this.events.All();
+        }
+
+        public IQueryable<Event> GetUpcomingEvents()
+        {
+            return this.events.All().Where(x => x.StartDateTime > DateTime.Now);
+        }
+
+        public IQueryable<Event> GetPassedEvents()
+        {
+            return this.events.All().Where(x => x.StartDateTime < DateTime.Now);
+        }
+
+        public Event GetEventById(int id)
+        {
+            return this.events.GetById(id);
+        }
+
+        public IQueryable<Event> GetRandomEvents(int count)
+        {
+            return this.events.All().OrderBy(x => Guid.NewGuid()).Take(count);
+        }
+
+        public void AddNewEvent(Event newEvent)
+        {
+            this.events.Add(newEvent);
+            this.events.Save();
+        }
+    }
+}
