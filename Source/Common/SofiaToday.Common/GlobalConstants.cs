a5a2dbb4daf31e14087bd25184718d28382879a9
Stoyan <stoyan.berov@gmail.com>
2016-02-23 16:00:15 +0200

    Fixed constants

--- Source/Common/SofiaToday.Common/GlobalConstants.cs
+++ Source/Common/SofiaToday.Common/GlobalConstants.cs
@@ -1,19 +1,19 @@
 ﻿namespace SofiaToday.Common
 {
-    public class GlobalConstants
+    public static class GlobalConstants
     {
         public const string AdministratorRoleName = "Administrator";
-        public const string ModeratorRoleName = "Moderator";
+        public const string ArticleAddedSuccess = "Article added successfully!";
         public const string BusinessRoleName = "Business";
-        public const string UserRoleName = "User";
-
-        public const string LogOutSuccess = "Successful Logout!";
-        public const string LogInSuccess = "Successful Login!";
-        public const string RegisterSuccess = "Registration successful!";
 
         public const string CommentAddedSuccess = "Thank you for your comment!";
 
         public const string EventAddedSuccess = "Event added successfully!";
-        public const string ArticleAddedSuccess = "Article added successfully!";
+        public const string LogInSuccess = "Successful Login!";
+
+        public const string LogOutSuccess = "Successful Logout!";
+        public const string ModeratorRoleName = "Moderator";
+        public const string RegisterSuccess = "Successful Registration!";
+        public const string UserRoleName = "User";
     }
 }
